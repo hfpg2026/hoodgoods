383c276f6095862aaff474cccd824dd8301135de
Jasmine <jasminishh@gmail.com>
2025-01-13 17:41:05 +0800

    Init stuff (#1)
    * feat: init db
    
    * chore: setup routers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import Link from 'next/link'
 import { LatestPost } from '@/app/_components/post'
 import { auth } from '@/server/auth'
-import { api, HydrateClient } from '@/trpc/server'
+import { HydrateClient } from '@/trpc/server'
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: 'from tRPC' })
   const session = await auth()
 
-  if (session?.user) {
-    void api.post.getLatest.prefetch()
-  }
+  // if (session?.user) {
+  // void api.post.getLatest.prefetch()
+  // }
 
   return (
     <HydrateClient>
@@ -43,9 +42,7 @@ export default async function Home() {
             </Link>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : 'Loading tRPC query...'}
-            </p>
+            <p className="text-2xl text-white">Hello</p>
 
             <div className="flex flex-col items-center justify-center gap-4">
               <p className="text-center text-2xl text-white">
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,8 @@
-import { postRouter } from '@/server/api/routers/post'
 import { createCallerFactory, createTRPCRouter } from '@/server/api/trpc'
+
+import { businessRouter } from './routers/business'
+import { tagRouter } from './routers/tag'
+import { userRouter } from './routers/user'
 
 /**
  * This is the primary router for your server.
@@ -7,7 +10,9 @@ import { createCallerFactory, createTRPCRouter } from '@/server/api/trpc'
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  business: businessRouter,
+  tag: tagRouter,
+  user: userRouter,
 })
 
 // export type definition of API
