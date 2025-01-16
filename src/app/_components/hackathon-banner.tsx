24bf4829eb1ad279823e293e9bea13e531db5b2a
Jasmine <jasminishh@gmail.com>
2025-01-16 12:11:23 +0800

    chore: banner

---
+++ src/app/_components/hackathon-banner.tsx
@@ -0,0 +1,8 @@
+export const HackathonBanner = () => {
+  return (
+    <div className="bg-dark-brown px-2 py-1 text-white">
+      ⚠️ HoodGoods is a work-in-progress Hackathon project for OGP&apos;s Hack
+      for Public Good
+    </div>
+  )
+}
