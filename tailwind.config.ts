e69757d6c6dd0e7cfb887a8c5c19dba76be013f3
LoneRifle <LoneRifle@users.noreply.github.com>
2025-01-22 15:19:07 +0800

    fix(tags): use flexi grid

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,6 +57,9 @@ export default {
         md: `calc(var(--radius) - 2px)`,
         sm: 'calc(var(--radius) - 4px)',
       },
+      gridTemplateColumns: {
+        tags: 'repeat(auto-fit, minmax(75px, 1fr))',
+      },
     },
   },
   plugins: [animations],
