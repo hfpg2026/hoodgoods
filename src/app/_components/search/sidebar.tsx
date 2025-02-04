35f725e87f6ed62919a81c864a239867a3ce9178
Jasmine <jasminishh@gmail.com>
2025-02-04 13:42:32 +0800

    fix: more mobile

--- src/app/_components/search/sidebar.tsx
+++ src/app/_components/search/sidebar.tsx
@@ -42,7 +42,7 @@ export const SearchSidebar = ({ tags }: { tags: Tag[] }) => {
   }, [selectedTags, router, searchParams])
 
   return (
-    <div className="flex w-full gap-4 px-8 md:max-w-[300px] md:flex-col">
+    <div className="flex w-full gap-4 px-8 md:max-w-[250px] md:flex-col lg:max-w-[300px]">
       <div className="flex w-full flex-col gap-2">
         <div className="font-bold">Categories</div>
         <div className="flex w-full flex-wrap gap-2 md:flex-col">
